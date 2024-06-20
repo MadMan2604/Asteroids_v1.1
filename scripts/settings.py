93d8d89702b7192cd808b967ecb80d04dc1d1dc0
MadMan2604 <madan.krishnan@student.education.wa.edu.au>
2024-06-20 12:45:44 +0800

    os path join update

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,18 +1,20 @@
+import os
+
 # game setup variables
 WIDTH = 1400
 HEIGHT = 800
 CAPTION = "Asteroids"
 FPS = 60
 
-# button dimentions
+# button dimensions
 BUTTON_WIDTH = 200
 BUTTON_HEIGHT = 50
 
 # fonts 
-FONT1 = "assets/fonts/ARCADE.ttf"
-FONT2 = "assets/fonts/Arial.ttf"
-FONT3 = "assets/fonts/Jersey-Slim.ttf"
-FONT4 = "assets/fonts/Slim-Thirteen-Pixel-Fonts.ttf"
+FONT1 = os.path.join("assets", "fonts", "ARCADE.ttf")
+FONT2 = os.path.join("assets", "fonts", "Arial.ttf")
+FONT3 = os.path.join("assets", "fonts", "Jersey-Slim.ttf")
+FONT4 = os.path.join("assets", "fonts", "Slim-Thirteen-Pixel-Fonts.ttf")
 
 # colours 
 WHITE = (255, 255, 255)
@@ -22,5 +24,5 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
 # filepaths
-BACKGROUND_PATH = "assets/images/background/"
-SPRITES = "assets/sprites/"
+BACKGROUND_PATH = os.path.join("assets", "images", "background/")
+SPRITES = os.path.join("assets", "sprites/")
